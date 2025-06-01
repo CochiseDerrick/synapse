05d92ead1c846f3cf72a0b57e2e1544ae55a4e1b
unknown <chise.derrick@gmail.com>
2025-06-01 19:58:03 +0100

    back to dist

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './build',
-    emptyOutDir: true, // also necessary
-  },
+  base: '/synapse/',
   plugins: [react()],
 })
